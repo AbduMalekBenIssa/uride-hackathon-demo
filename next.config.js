97364898089b8f4b09c7a760af18b58110b2a054
AbduMalek <abdumalek.benissa@ucalgary.ca>
2025-04-26 17:31:10 -0600

    fix: Remove deprecated experimental.appDir option

--- next.config.js
+++ next.config.js
@@ -3,9 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: "standalone",
-  experimental: {
-    appDir: true,
-  },
+  // experimental: {
+  //   appDir: true,
+  // },
 }
 
 module.exports = nextConfig
